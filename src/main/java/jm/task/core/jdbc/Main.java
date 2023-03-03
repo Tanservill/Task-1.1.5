56bbb7df097fe7e4e4e3feb5b9601e7622b9ab74
Ralnikov Denis <dentreyt2909@gmail.com>
2023-03-03 10:57:51 +0300

    Задача выполнена 3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,35 +2,26 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
-        Util.connection().setAutoCommit(false);
 
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
-        Util.connection().commit();
 
         userService.saveUser("Name1", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
-        Util.connection().commit();
 
         userService.removeUserById(1);
-        Util.connection().commit();
 
         userService.getAllUsers();
 
         userService.cleanUsersTable();
-        Util.connection().commit();
 
-        Util.connection().close();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,36 +16,48 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try (Statement statement = Util.connection().createStatement();){
-            String SQL = "CREATE TABLE IF NOT EXISTS User (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age INT)";
+        try (Connection connection = Util.connection();){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            String SQL = "CREATE TABLE IF NOT EXISTS User (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age TINYINT)";
             statement.executeUpdate(SQL);
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = Util.connection().createStatement();){
+        try (Connection connection = Util.connection();){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
             String SQL = "DROP TABLE IF EXISTS User";
             statement.executeUpdate(SQL);
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Statement statement = Util.connection().createStatement();){
+        try (Connection connection = Util.connection();){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
             String SQL = "INSERT INTO User (name, lastName, age) VALUES ("+ "'" + name + "', " + "'"+ lastName + "', " + age + ")";
             statement.executeUpdate(SQL);
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = Util.connection().createStatement();){
+        try (Connection connection = Util.connection();){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
             String SQL = "DELETE  FROM User WHERE id = "+ id ;
             statement.executeUpdate(SQL);
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -53,7 +66,8 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List <User> users = new ArrayList<>();
 
-        try (Statement statement = Util.connection().createStatement();){
+        try (Connection connection = Util.connection();){
+            Statement statement = connection.createStatement();
             String SQL = "SELECT * FROM User";
             ResultSet set = statement.executeQuery(SQL);
             while (set.next()){
@@ -73,9 +87,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = Util.connection().createStatement();){
+        try (Connection connection = Util.connection();){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
             String SQL = "TRUNCATE TABLE User";
             statement.executeUpdate(SQL);
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
